d1a4741fe754f7128a02878450e494d23c1b1a2e
Mr-TanNguyen <33664611+Mr-TanNguyen@users.noreply.github.com>
2021-04-28 00:11:32 +0700

    update 27/4

---
+++ Base_FE/src/main/webapp/app/core/services/room-api/room-api-service.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { SERVER_API } from 'app/shared/constants/api-resource.constants';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RoomApiServiceService {
+  private baseUri = SERVER_API;
+  private token = localStorage.getItem('token');
+
+  constructor(private http: HttpClient) {}
+
+  searchRoom(searchForm?: any): Observable<any> {
+    return this.http.post(this.baseUri + '/room/searchRoom', searchForm);
+  }
+
+  save(data): Observable<any> {
+    return this.http.post<any>(SERVER_API + '/room/add', data);
+  }
+
+  deleteRoom(id): Observable<any> {
+    return this.http.get<any>(SERVER_API + '/room/deleteRoom/' + id);
+  }
+
+  getInfo(Id): Observable<any> {
+    return this.http.get(SERVER_API + '/room/get-room-by-id/' + Id);
+  }
+}
