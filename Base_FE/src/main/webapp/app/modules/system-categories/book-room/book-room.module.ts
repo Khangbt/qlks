8e09ae2dd57c87e386341f46b5225967a5943f9d
Khangbt <buithekhang246@gmail.com>
2021-04-28 20:02:42 +0700

    update

---
+++ Base_FE/src/main/webapp/app/modules/system-categories/book-room/book-room.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TreeViewModule } from '@syncfusion/ej2-angular-navigations';
+import { ChartsModule } from 'ng2-charts';
+import { InvoiceWebappSharedModule } from 'app/shared/shared.module';
+import { PerfectScrollbarModule } from 'ngx-perfect-scrollbar';
+import { AssetResourcesRoutingModule } from 'app/modules/system-categories/asset-resuorce/asset-resources-routing.module';
+import { BookRoomComponent } from './book-room.component';
+import { AddBookRoomComponent } from './add-book-room/add-book-room.component';
+// import { AddAssetComponent } from './add-asset/add-asset.component';
+
+@NgModule({
+  declarations: [AddBookRoomComponent, BookRoomComponent, AddBookRoomComponent],
+  imports: [CommonModule, PerfectScrollbarModule, InvoiceWebappSharedModule, ChartsModule, TreeViewModule, AssetResourcesRoutingModule],
+  entryComponents: [AddBookRoomComponent]
+})
+export class BookRoomModule {}
--- Base_FE/src/main/webapp/app/modules/system-categories/room/room-resources.module.ts
+++ Base_FE/src/main/webapp/app/modules/system-categories/room/room-resources.module.ts
@@ -4,12 +4,14 @@ import { TreeViewModule } from '@syncfusion/ej2-angular-navigations';
 import { ChartsModule } from 'ng2-charts';
 import { InvoiceWebappSharedModule } from 'app/shared/shared.module';
 import { PerfectScrollbarModule } from 'ngx-perfect-scrollbar';
-import { AddRoomComponent } from 'app/modules/system-categories/room/add-room/add-room.component';
+// import { AddRoomComponent } from 'app/modules/system-categories/room/add-room/add-room.component';
 import { RoomResourcesRoutingModule } from 'app/modules/system-categories/room/room-resources-routing.module';
 
 @NgModule({
   declarations: [],
   imports: [CommonModule, PerfectScrollbarModule, InvoiceWebappSharedModule, ChartsModule, TreeViewModule, RoomResourcesRoutingModule],
-  entryComponents: [AddRoomComponent]
+  entryComponents: [
+    // AddRoomComponent
+  ]
 })
 export class RoomResourcesModule {}
